cefceac0101d8df8931ab2f782a1228c351ecd6b
Darvis <dsouzavishal685@gmail.com>
2026-04-26 08:12:54 +0530

    Update Kotlin and IntelliJ plugin versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,14 @@ rootProject.name = "c3intellij"
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "2.1.20"
+        id("org.jetbrains.kotlin.jvm") version "2.3.21"
         id("org.jetbrains.changelog") version "2.5.0"
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("org.jetbrains.intellij.platform.settings") version "2.15.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.14.0"
 }
 
 @Suppress("UnstableApiUsage")
