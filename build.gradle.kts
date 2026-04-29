64bd9ed75675f05f790d8dff859bb4eb56ff5251
Darvis <dsouzavishal685@gmail.com>
2026-04-29 08:18:33 +0530

    Add LSP4J dependency to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ sourceSets {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
+    implementation("org.eclipse.lsp4j:org.eclipse.lsp4j:0.24.0")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
